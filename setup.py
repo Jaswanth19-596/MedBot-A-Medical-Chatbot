9478801a42159c6483063856b2c8f02a14b03860
Jaswanth <madhajaswanth@gmail.com>
2025-11-26 00:34:11 -0600

    Added all the requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "medical_chatbot",
+    version = "0.1",
+    author = "Jaswanth Mada",
+    author_email= "madhajaswanth@gmail.com",
+    description="A medical chatbot using LLMs",
+    packages = find_packages(),
+    install_requires = []
+)

7db8363240dc1585fc052e0f87c5188a5f8bf3e3
Jaswanth <madhajaswanth@gmail.com>
2025-11-26 00:11:01 -0600

    Initial Project Setup Done

---
+++ template.sh
@@ -0,0 +1,16 @@
+
+# Creating directories
+mkdir -p src
+mkdir -p research
+
+# Creating Files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/experiment.ipynb
+touch requirements.txt
+
+echo "Directories and Files Created successfully"
